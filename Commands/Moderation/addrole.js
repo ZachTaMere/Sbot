a97223ca80e0be4bd91f32b630bcb6f0307ede38
Zach <zamaru33@gmail.com>
2021-11-18 14:38:20 +0100

    Update addrole.js
    Added :
    - Check if the target is the bot
    - Check if the bot has a higher role than the role that wants to be added
    - Added some comments
    
    Fixed :
    - The error where "size" was written "siza" at line 33

--- Commands/Moderation/addrole.js
+++ Commands/Moderation/addrole.js
@@ -1,4 +1,4 @@
-const { CommandInteraction, MessageEmbed, User, ClientUser } = require("discord.js");
+const { CommandInteraction, MessageEmbed, Client } = require("discord.js");
 const { execute } = require("../../Events/ready");
 
 module.exports = {
@@ -30,24 +30,40 @@ module.exports = {
 
         const response = new MessageEmbed()
             .setColor("RANDOM")
-            .setAuthor(interaction.user.username, interaction.user.displayAvatarURL({ dynamic: true, siza: 512}))
+            .setAuthor(interaction.user.username, interaction.user.displayAvatarURL({ dynamic: true, size: 512 }))
             .setThumbnail(target.displayAvatarURL({dynamic: true, siza: 512}))
+            .setTimestamp()
+            .setFooter(client.user.username, client.user.displayAvatarURL({ dynamic: true, size:512 }))
 
+        // check if the ${target} is the bot
+        if (target === client.user) {
+            response.setDescription("Tu ne peux pas m'ajouter de rôle !")
+            return interaction.reply({ embeds: [response] });  
+        }
+
+        // check if the member has a higher role than the ${Role}
         if (Role.position > member.roles.highest.position) {
             response.setDescription("Tu ne peux ajouter des rôles plus haut que toi.")
             return interaction.reply({ embeds: [response] });
         }
 
+        // check if the bot has a higher role than the ${Role}
+        if (Role.position > guild.members.resolve(client.user).roles.highest.position) {
+            response.setDescription("Je ne peux pas ajouter des rôles plus haut que moi.")
+            return interaction.reply({ embeds: [response] });
+        }
+
+        // send a message in the channel where the command was involved 
         response.setDescription(`Tu as bien ajouter le rôle ${Role} à ${target}`);
         interaction.reply({ embeds: [response] })
 
-        
+        // add the role to the ${target} and catch the error if there's one
         target.roles.add(Role)
         .catch((error) => { console.log(error) });
 
+        // send a message in the log channel
         response.setDescription(`L'utilisateur ${interaction.user} à ajouter le rôle ${Role} à ${target}`)
         guild.channels.cache.get("909915653547376672").send({embeds: [response] });
        
-
     }
 }
